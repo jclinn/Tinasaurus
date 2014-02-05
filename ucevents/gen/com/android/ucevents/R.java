23d5630a88e2e69357f1b08e493291529cd8f41d
Judy Lin <jclinn@gmail.com>
2014-02-05 12:04:56 -0800

    ucevents base

--- ucevents/gen/com/android/ucevents/R.java
+++ ucevents/gen/com/android/ucevents/R.java
@@ -10,6 +10,9 @@ package com.android.ucevents;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue=0x7f060000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -22,18 +25,20 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int login=0x7f080001;
-        public static final int logo=0x7f080000;
-        public static final int signUp=0x7f080002;
+        public static final int action_settings=0x7f090004;
+        public static final int groupname=0x7f090003;
+        public static final int login=0x7f090001;
+        public static final int logo=0x7f090000;
+        public static final int signUp=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -43,6 +48,7 @@ public final class R {
         public static final int hello_world=0x7f050002;
         public static final int login=0x7f050003;
         public static final int signup=0x7f050004;
+        public static final int ucevents=0x7f050007;
     }
     public static final class style {
         /** 
@@ -66,10 +72,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
